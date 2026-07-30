57ff2ac926b809856ebb8ee2d8788e57f88ea920
nayaracalenzo <queiroz.14@hotmail.com>
2026-07-30 20:09:32 -0300

    add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css'
-import { Logo } from './components/Logo/Logo'
-import SideBar from './components/SideBar'
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import Dashboard from "./pages/Dashboard";
+// import { Logo } from './components/Logo/Logo'
+// import SideBar from './components/SideBar'
 
 function App() {
 
   return (
     <>
-      <Logo variant="outline" color="claro" size="md">
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage/>} /> 
+        <Route path="/cadastro" element={<RegisterPage/>} /> 
+        <Route path="/home" element={<Dashboard/>} /> 
+      </Routes>
+    </BrowserRouter>
+
+
+
+
+
+
+
+
+
+      {/* <Logo variant="outline" color="claro" size="md">
         <Logo.Icon />
         <Logo.Text />
       </Logo>
@@ -19,13 +40,13 @@ function App() {
       <Logo variant="outline" color="claro" size="lg">
         <Logo.Icon />
         {/* <Logo.Text /> */}
-      </Logo>
+      {/* </Logo>
 
       <Logo variant="filled" color="roxo" size="lg">
         <Logo.Icon />
         <Logo.Text />
       </Logo>
-      <SideBar/>
+      <SideBar/> */} 
     </>
   );
 }
