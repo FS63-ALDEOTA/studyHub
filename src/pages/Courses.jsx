70253075f49acc7db0f8af8aa4b98ddbffc69841
R8152 <raulteles05@gmail.com>
2026-08-06 22:01:36 -0300

    style começar a decorar links

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom"
 const Courses = () => {
   return (
-    <div>
-      <Link className="">Visão Geral</Link>
-      <Link className="">Materiais</Link>
-      <Link className="">Conteúdo</Link>
-      <Link className="">Avaliações</Link>
+    <div className="ml-6">
+      <Link className="mr-8 my-4 text-[14px]">Visão Geral</Link>
+      <Link className="mr-8 my-4 text-[14px]">Conteúdo</Link>
+      <Link className="mr-8 my-4 text-[14px]">Materiais</Link>
+      <Link className="my-4 text-[14px]">Avaliações</Link>
     </div>
   )
 }
