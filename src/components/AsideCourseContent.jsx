822c8bd4b2fa45f090004c79de22d4484306134c
DomingosMt <domingosmuratori@outlook.com>
2026-08-06 21:29:55 -0300

    feature: AsideCourseContent file

---
+++ src/components/AsideCourseContent.jsx
@@ -0,0 +1,14 @@
+
+function AsideCourseContent() {
+    return (
+        <div>
+            <h1>Conteúdo do Curso</h1>
+            <div>
+                <span>Progresso</span>
+                <span>45% concluído</span>
+            </div>
+        </div>
+    )
+}
+
+export default AsideCourseContent
