63474d85702b58d08a1f232f878dc86894db17a0
R8152 <raulteles05@gmail.com>
2026-08-11 14:51:54 -0300

    style: finaliza a estilização

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 const Courses = () => {
   return (
-    <div className="ml-57.5 border-b-2 border-[#CCC3D8] pl-6 pb-6">
+    <div className="border-b-2 border-[#CCC3D8] pl-6 pb-6">
       <Link className="mr-8 my-4 text-[14px] focus: text-primary focus: border-b-2 pb-6.5">Visão Geral</Link>
       <Link className="mr-8 my-4 text-[14px] focus: text-primary focus: border-b-2 pb-6.5">Conteúdo</Link>
       <Link className="mr-8 my-4 text-[14px] focus: text-primary focus: border-b-2 pb-6.5">Materiais</Link>
