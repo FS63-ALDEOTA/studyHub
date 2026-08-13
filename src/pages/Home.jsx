38f5b5a2401ef0e784aa1b7e42e202a7d79efe03
carloscandiido <carloscandido1999@gmail.com>
2026-08-13 00:17:23 -0300

    SummaryCards Completo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,25 @@
-import SummaryCard from "../components/SummaryCard.jsx"
-
+import SummaryCards from "../components/SummaryCards"
+import { infos } from "./infos"
 
 const Home = () => {
-  return (
-    <div>
-      <h1>Página inicial</h1>
+    return (
+        <div>
+            <h1>Página inicial</h1>
 
-
-        <SummaryCard/>
-
-    </div>
-  )
+            <div className="flex gap-4">
+                {infos.map((info) => (
+                    <SummaryCards
+                        key={info.title}
+                        title={info.title}
+                        subtitle={info.subtitle}
+                        icon={info.icon}
+                        colorBgIcon={info.colorBgIcon}
+                        colorIcon={info.colorIcon}
+                    />
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default Home
