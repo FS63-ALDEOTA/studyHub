833b2f189175f46e41c06d20c0491239126aad7d
WilliamBritoFreitas <carteirodowilliam@gmail.com>
2026-08-13 02:35:48 -0300

    banner sem professores

--- src/App.jsx
+++ src/App.jsx
@@ -11,25 +11,30 @@ import Favorites from "./pages/Favorites";
 import User from "./pages/User";
 import Home from "./pages/Home";
 
+
 function App() {
 
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage/>} /> 
-        <Route path="/cadastro" element={<RegisterPage/>} /> 
-        <Route element={<Dashboard/>} > 
-          <Route path="/home" element={<Home/>} /> 
-          <Route path="/meus-cursos" element={<Courses/>} /> 
-          <Route path="/atividades" element={<Activities/>} /> 
-          <Route path="/calendario" element={<Calendar/>} /> 
-          <Route path="/perfil" element={<User/>} /> 
-          <Route path="/favoritos" element={<Favorites/>} /> 
-          <Route path="/configuracoes" element={<Configurations/>} />
-        </Route> 
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Courses />} />
+
+          {/* <Route path="/" element={<LoginPage />} /> */}
+
+
+          <Route path="/cadastro" element={<RegisterPage />} />
+          <Route element={<Dashboard />}>
+            <Route path="/home" element={<Home />} />
+            <Route path="/meus-cursos" element={<Courses />} />
+            <Route path="/atividades" element={<Activities />} />
+            <Route path="/calendario" element={<Calendar />} />
+            <Route path="/perfil" element={<User />} />
+            <Route path="/favoritos" element={<Favorites />} />
+            <Route path="/configuracoes" element={<Configurations />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,89 @@
+import { createContext,useContext } from 'react';
+
+import LogoIcon_Outline from '../../assets/logos/capelo_outline.svg?react';
+import LogoIcon_Filled from "../../assets/logos/capelo_filled.svg?react";
+
+const LogoPropriedades = createContext(null);
+
+export function Logo({children, variant = "outline", color = "roxo", size = "md", className = "",}) {
+  const bgColors = {
+    roxo: "bg-purple-600 text-white",
+    escuro: "bg-gray-900 text-white",
+    claro: "bg-white text-purple-600",
+  };
+
+
+return (
+       <LogoPropriedades.Provider value={{ variant, color, size }}>
+        {/* <div className={`flex items-center gap-3 font-sans select-none p-4 rounded-xl ${bgColors[color] || ''} ${className}`}> */}
+      <div className={`flex items-center gap-3 font-sans select-none p-4  ${className}`}>
+        {children}
+      </div>
+    </LogoPropriedades.Provider>
+  );
+}
+
+// Icone Opções
+
+Logo.Icon = function LogoIconComponent({className=''}) {
+    const {variant,color, size} = useContext (LogoPropriedades);
+
+    const sizes = {
+      sm: "w-8 h-8 rounded-lg p-1.5",
+      md: "w-11 h-11 rounded-xl p-2.5", 
+      lg: "w-16 h-16 rounded-2xl p-3.5",
+    };
+
+    const iconColors = {
+      roxo: "text-white",
+      claro: "text-purple-600",
+    };
+
+    const bgIconColors = {
+      roxo: "bg-purple-600",
+      claro: "bg-white",
+    };
+
+    return (
+      <div
+        className={`${sizes[size] || sizes.md} ${iconColors[color] || "text-purple-600"} ${bgIconColors[color] || "text-purple-600"} ${className}`}
+      >
+        {variant === "outline" ? (
+          <LogoIcon_Outline className="w-full h-full object-contain" />
+        ) : (
+          <LogoIcon_Filled className="w-full h-full object-contain" />
+        )}
+      </div>
+    );
+};
+
+// Texto do Logo
+
+Logo.Text = function LogoTextComponent({ className = "" }) {
+ 
+  const { variant, color, size } = useContext(LogoPropriedades);
+
+  const textSizes = {
+    sm: "text-base tracking-tight",
+    md: "text-2xl tracking-tight",
+    lg: "text-4xl ttracking-tight",
+  };
+
+
+  const textColors = {
+    roxo: "text-purple-600",
+    claro: "text-purple-600",
+  };
+
+ 
+  const fontWeight = variant === "outline" ? "font-medium" : "font-bold";
+
+  return (
+    <span
+      className={`${fontWeight} ${textColors[color] || "text-purple-900"} ${textSizes[size] || textSizes.md} ${className}`}
+    >
+      StudyHub
+    </span>
+  );
+};
+
