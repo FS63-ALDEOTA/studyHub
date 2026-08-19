9e2bd88dd8ba3e64762c4b98c0cff57b8e883f96
R8152 <raulteles05@gmail.com>
2026-08-19 20:48:14 -0300

    fix: focus

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom"
 const Courses = () => {
   return (
     <div className="border-b-2 border-[#CCC3D8] pl-6 pb-6">
-      <Link className="mr-8 my-4 text-[14px] focus: text-primary focus: border-b-2 pb-6.5">Visão Geral</Link>
-      <Link className="mr-8 my-4 text-[14px] focus: text-primary focus: border-b-2 pb-6.5">Conteúdo</Link>
-      <Link className="mr-8 my-4 text-[14px] focus: text-primary focus: border-b-2 pb-6.5">Materiais</Link>
-      <Link className="my-4 text-[14px] focus: text-primary focus: border-b-2 pb-6.5">Avaliações</Link>
+      <Link className="mr-8 my-4 text-[14px] focus:text-primary focus:border-b-2 pb-6.5">Visão Geral</Link>
+      <Link className="mr-8 my-4 text-[14px] focus:text-primary focus:border-b-2 pb-6.5">Conteúdo</Link>
+      <Link className="mr-8 my-4 text-[14px] focus:text-primary focus:border-b-2 pb-6.5">Materiais</Link>
+      <Link className="my-4 text-[14px] focus:text-primary focus:border-b-2 pb-6.5">Avaliações</Link>
     </div>
   )
 }
