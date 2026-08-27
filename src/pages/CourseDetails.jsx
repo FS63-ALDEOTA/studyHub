c167b6470437ad2914c8d5a16fb72a25ced12fcc
nayaracalenzo <queiroz.14@hotmail.com>
2026-08-27 20:48:56 -0300

    adjust aside render

---
+++ src/pages/CourseDetails.jsx
@@ -0,0 +1,11 @@
+import AsideCourseContent from "../components/AsideCourseContent.jsx"
+
+const CourseDetails = () => {
+  return (
+    <div>
+      <AsideCourseContent/>
+    </div>
+  )
+}
+
+export default CourseDetails
