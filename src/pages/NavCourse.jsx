f2f1630b964bea27592386af717c833aa5eed8af
nayaracalenzo <queiroz.14@hotmail.com>
2026-08-27 21:26:20 -0300

    fix-route-outlet

--- src/pages/NavCourse.jsx
+++ src/pages/NavCourse.jsx
@@ -7,7 +7,7 @@ export default function NavCourse() {
   return (
     <div>
       <div className='border-b-2 p-4 border-[#CCC3D8]'>
-        <NavLink className={({isActive}) =>` mr-8 my-4 text-[14px] pb-4 ${isActive ? "text-primary border-b-2 border-primary" : ""}`} to={"geral_curso"}>Visão Geral</NavLink>
+        <NavLink className={({isActive}) =>` mr-8 my-4 text-[14px] pb-4 ${isActive ? "text-primary border-b-2 border-primary" : ""}`} to="." end>Visão Geral</NavLink>
         <NavLink className={({isActive}) =>`mr-8 my-4 text-[14px] pb-4 ${isActive ? "text-primary border-b-2 border-primary" : ""}`} to={"conteudo"}>Conteúdo</NavLink>
         <NavLink className={({isActive}) =>`mr-8 my-4 text-[14px] pb-4 ${isActive ? "text-primary border-b-2 border-primary" : ""}`} to={"materiais"}>Materiais</NavLink>
         <NavLink className={({isActive}) =>`mr-8 my-4 text-[14px] pb-4 ${isActive ? "text-primary border-b-2 border-primary" : ""}`} to={"avaliacoes"}>Avaliações</NavLink>
