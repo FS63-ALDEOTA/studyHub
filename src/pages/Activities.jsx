5d6103b332ccc7f5dfd516296686578515d17040
nayaracalenzo <queiroz.14@hotmail.com>
2026-09-03 18:46:42 -0300

    adjust modulecard

--- src/pages/Activities.jsx
+++ src/pages/Activities.jsx
@@ -1,48 +1,49 @@
-import {useEffect, useState } from "react"
+// import {useEffect, useState } from "react"
 
 const Activities = () => {
-  const [atividades, setAtividades] = useState([])
-  const [novaAtividade, setNovaAtividade] = useState("")
+//   const [atividades, setAtividades] = useState([])
+//   const [novaAtividade, setNovaAtividade] = useState("")
 
 
-  function buscarAtividades () {
-    fetch("http://localhost:3000/atividades")
-      .then(res => res.json())
-      .then(dados => setAtividades(dados))
-      .catch(error => console.error(error))
-  }
+//   function buscarAtividades () {
+//     fetch("http://localhost:3000/atividades")
+//       .then(res => res.json())
+//       .then(dados => setAtividades(dados))
+//       .catch(error => console.error(error))
+//   }
   
-useEffect(()=> {
-  buscarAtividades()
-})
+// useEffect(()=> {
+//   buscarAtividades()
+// })
 
 
 
 
-  function handleAddAtividade () {
-    const payload = {nome: novaAtividade}
+//   function handleAddAtividade () {
+//     const payload = {nome: novaAtividade}
 
-    fetch("http://localhost:3000/atividades", {
-      method: "POST",  //CREATE
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(payload)
-    })
-      .then(res => res.json())
-      .then(dados => {
-        console.log(dados)
-        setNovaAtividade("")
-        buscarAtividades()
-      }
-    )
-      .catch(error => console.error(error))
-  }
+//     fetch("http://localhost:3000/atividades", {
+//       method: "POST",  //CREATE
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(payload)
+//     })
+//       .then(res => res.json())
+//       .then(dados => {
+//         console.log(dados)
+//         setNovaAtividade("")
+//         buscarAtividades()
+//       }
+//     )
+//       .catch(error => console.error(error))
+//   }
   
   
   return (
     <div>
-      <h1 className="font-bold p-2">Atividades</h1>
+      <h1>Atividades</h1>
+      {/* <h1 className="font-bold p-2">Atividades</h1>
       <ul>
         {atividades.map((item)=> (
             <li key={item.id}>{item.nome}</li>
@@ -51,7 +52,7 @@ useEffect(()=> {
       <div className="bg-purple-400 h-screen">
       <input className="border p-2 mr-2" type="text" value={novaAtividade} onChange={(e) => setNovaAtividade(e.target.value)} name="nome"/>
       <button onClick={handleAddAtividade}>Criar atividade</button>
-      </div>
+      </div> */}
     </div>
   )
 }
